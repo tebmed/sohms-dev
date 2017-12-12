fb995493f2a2be5a6953bd67e0777354b8df3bed
LaurentGirard <laurent.girard44620@gmail.com>
2017-12-12 15:43:10 +0100

    ajout MockArena

---
+++ CapstoneProject/src/Communication/MocArena.java
@@ -0,0 +1,37 @@
+package Communication;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MocArena {
+
+	public static void main(String[] args) throws InterruptedException {
+		ServerSocket serv;
+		Socket client;
+		
+		BufferedReader reader;
+		PrintStream writer;
+
+		try {
+			serv = new ServerSocket(1202);
+			client = serv.accept();
+			reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			writer = new PrintStream(client.getOutputStream());
+			
+			String message;
+			while(true) {
+				message = reader.readLine();
+				writer.println("OK:" + message);
+				writer.println("START:" + message);
+				
+				writer.println("END:" + message);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
