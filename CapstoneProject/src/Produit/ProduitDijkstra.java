a5c6a6b97cd52466d9a1cfce9981c35043456e5a
pierreLmty <lemetayer.pierre44@gmail.com>
2017-12-27 18:12:44 +0100

    debug

--- CapstoneProject/src/Produit/ProduitDijkstra.java
+++ CapstoneProject/src/Produit/ProduitDijkstra.java
@@ -3,11 +3,10 @@ package Produit;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import Communication.ListenerArena;
 
 public class ProduitDijkstra{
 
@@ -26,6 +25,13 @@ public class ProduitDijkstra{
 		graphe = p.parse(obj);
 	}
 	
+	/**
+	 * Détermine tous les chemins possibles pour aller du point start et point end
+	 * @param start l'id du noeud de départ
+	 * @param end l'id du noeud de fin
+	 * @param possiblePath La liste de segment qui détermine le chemin
+	 * @param cout le cout du chemin
+	 */
 	public void applyDijkstra(int start, int end, ArrayList<Integer> possiblePath, int cout)
 	{			
 		//Si il est présent dans la liste possiblePath
@@ -50,6 +56,27 @@ public class ProduitDijkstra{
 				applyDijkstra(s.getNodeTo(), end, possiblePath, cout);
 			}
 		}
+		
+		
 	}
 
+	public ArrayList<Integer> bestPath(){
+		
+		int cout = 10000;
+		ArrayList<Integer> path = null;
+		ArrayList<Integer> cle;
+		int valeur;
+		
+		for(Entry<ArrayList<Integer>, Integer> entry : possiblePaths.entrySet()) {
+			cle = entry.getKey();
+		    valeur = entry.getValue();
+		    
+		    if(valeur <= cout) {
+		    	cout = valeur;
+		    	path = cle;
+		    }
+		}
+		
+		return path;
+	}
 }
