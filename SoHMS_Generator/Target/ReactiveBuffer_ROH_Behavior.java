0ce909eeb2331d894d94467589606e978668291f
tebmed <lamin.teb94@gmail.com>
2018-06-04 19:33:45 +0200

    Add workshop model to atl grammar

---
+++ SoHMS_Generator/Target/ReactiveBuffer_ROH_Behavior.java
@@ -0,0 +1,36 @@
+package Target;
+//Med Generator 
+public class ReactiveBuffer_ROH_Behavior extends ROH_Behavior{
+   
+  protected  ResourceHolon  linkedOutputRH;
+  public  int  x;
+ 
+  public   MServiceSpecification  requestServiceExe(){
+  }
+}
+
+
+
+
+
+
+
+public class ResourceHolon{
+   
+ 
+}public class ROH_Behavior{
+   
+ 
+}public class MServiceSpecification{
+   
+ 
+}public class PathState{
+   
+ 
+}public class ROH{
+   
+ 
+}public class DirectoryFacilitator{
+   
+ 
+}
---
+++ SoHMS_Generator/Target/hello.java
@@ -0,0 +1,36 @@
+public class ReactiveBuffer_ROH_Behavior extends ROH_Behavior{
+   
+  public  ResourceHolon  linkedOutputRH;
+  public  int  x;
+ 
+  public   MServiceSpecification  requestServiceExe(ProductHolon  client,PathState  prodTask,ROH  roh,DirectoryFacilitator  df,){
+  }
+  public   void  requestPortPermition(ProductHolon  client,String  finalPort,long  timeFromNow,ROH  roh){
+  }
+  public   void  requestDefaultTransfer( ){
+  }
+}
+
+
+public class ResourceHolon{
+   
+ 
+}public class ROH_Behavior{
+   
+ 
+}public class MServiceSpecification{
+   
+ 
+}public class PathState{
+   
+ 
+}public class ROH{
+   
+ 
+}public class DirectoryFacilitator{
+   
+ 
+}public class ProductHolon{
+   
+ 
+}
